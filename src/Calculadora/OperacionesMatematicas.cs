90aae2f308413a4d92fd7dd9bdb5b4676bfc5e27
Luis Ixba <lixba@uv.mx>
2026-03-11 13:11:27 -0600

    worlfow

---
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -0,0 +1,14 @@
+namespace Calculadora;
+
+public class OperacionesMatematicas
+{
+    public int Sumar(int a, int b) => a + b;
+    public int Restar(int a, int b) => a - b;
+    public int Multiplicar(int a, int b) => a * b;
+    public double Dividir(int a, int b) {
+        if (b == 0)
+            throw new DivideByZeroException("no se puede dividir por cero");
+        return a / b;
+    }
+    public bool EsPar(int a) => a % 2 == 0;
+}
