938c17bf734ed255a35f73a50b013a537832fe7e
Luis Alberto Ixba Alarcón <141351770+lixba@users.noreply.github.com>
2026-03-11 13:26:29 -0600

    Workflows (#1)
    * worlfow
    
    * flujo pull request
    
    * flujo pull request 2

---
+++ tests/Calculadora.Tests/OperacionesMatematicasTests.cs
@@ -0,0 +1,48 @@
+using Calculadora;
+namespace Calculadora.Tests;
+
+public class OperacionesMatematicasTests
+{
+    private readonly OperacionesMatematicas _operacionesMatematicas = new();
+
+    [Fact]
+    public void Sumar_DosPositivos_RetornaLaSuma()
+    {
+        //arrange       
+        int a = 3 ;
+        int b = 5;
+        
+        //act
+        var resultado = _operacionesMatematicas.Sumar(a, b);
+        
+        //assert
+        Assert.Equal(8, resultado);
+    }
+
+    [Fact]
+    public void Sumar_NegativoMasPositivo_RegresaValorCorrecto()
+    {
+        
+        int a = -3 ;
+        int b = 5;
+        
+        //act
+        var resultado = _operacionesMatematicas.Sumar(a, b);
+        
+        //assert
+        Assert.Equal(2, resultado);
+    }
+
+    [Fact]
+    public void Sumar_ceroMasCero_regresaCero()
+    {
+        int a = 0 ;
+        int b = 0;
+        
+        //act
+        var resultado = _operacionesMatematicas.Sumar(a, b);
+        
+        //assert
+        Assert.Equal(0, resultado);
+    }
+}
