0653f5cb68bd1a22e4cc0939cd7b15dddda645e4
Luis Ixba <lixba@uv.mx>
2026-03-11 14:15:26 -0600

    prueba test error

--- src/Calculadora/OperacionesMatematicas.cs
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -2,7 +2,7 @@ namespace Calculadora;
 
 public class OperacionesMatematicas
 {
-    public int Sumar(int a, int b) => a + b;
+    public int Sumar(int a, int b) => a - b;
     public int Restar(int a, int b) => a - b;
     public int Multiplicar(int a, int b) => a * b;
     public double Dividir(int a, int b) {
